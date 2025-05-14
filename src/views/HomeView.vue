136689224b8dea33a5460ebb5332c17afc591b13
victor <VGeneralov@sila.ru>
2025-05-14 15:02:38 +0200

    feat: список на основной странице

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-
 </script>
 
 <template>
   <div class="w-full p-10 flex items-center justify-center ">
-    huy
+    <div class="w-full grid grid-cols-4 gap-20">
+      <div v-for="i in 40" class="w-full h-full bg-green-50 rounded border-1 border-gray-200 flex p-10 items-center justify-center" :key="i">huy {{i}}</div>
+    </div>
   </div>
 </template>
