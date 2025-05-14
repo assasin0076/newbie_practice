d16597eb656baede01a391ff05dd29de556b8d4d
victor <VGeneralov@sila.ru>
2025-05-14 16:21:11 +0200

    feat: генерация айди

--- src/composables/tasksComposable.ts
+++ src/composables/tasksComposable.ts
@@ -1,22 +1,30 @@
 import { Ref, ref } from 'vue'
+import generateId from '@/helpers/generateId'
 
 const tasks: Ref<TTask[]> = ref([])
 
 type TTask = {
   text: string
-  id: number
+  id: number | string
   time?: number
 }
 
 export const useTasksComposable = () => {
-  const addTask = (task: TTask) => {
-    tasks.value.push(task)
-    setTimeout(()=>{removeTask(task.id)}, 5000)
+  const addTask = (task: Omit<TTask, 'id'>) => {
+    const completeTask = {
+      ...task,
+      id: generateId(),
+    }
+
+    tasks.value.push(completeTask)
+
+    setTimeout(() => {
+      removeTask(completeTask.id)
+    }, 5000)
   }
 
-  const removeTask = (id: number) => {
+  const removeTask = (id: number | string) => {
     tasks.value = tasks.value.filter((task) => task.id !== id)
-
   }
   const removeAllTasks = () => {
     tasks.value = []
