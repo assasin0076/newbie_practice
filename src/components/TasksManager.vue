c6bac3ec6d6121e198cdc9cfae6579e582005c6c
CUMBOOY <buravlev.1998@mail.ru>
2025-05-19 17:55:14 +0400

    feat: уведомление при авторизации

--- src/components/TasksManager.vue
+++ src/components/TasksManager.vue
@@ -7,7 +7,7 @@ const tasks = tasksComposable.tasks;
 </script>
 
 <template>
-  <div class="absolute bottom-0 right-0 flex flex-col p-4 w-[400px] gap-y-3">
+  <div class="fixed bottom-0 right-0 flex flex-col p-4 w-[400px] gap-y-3">
     <div
       v-for="task in tasks"
       :key="task.id"
