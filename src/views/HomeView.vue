78434be76d5c006dd4ab556186142350a31ea0b5
CUMBOOY <buravlev.1998@mail.ru>
2025-05-28 01:04:56 +0400

    feat: сырой сайдбар

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,16 @@
 </script>
 
 <template>
-  <div class="w-full p-10 flex items-center justify-center ">
-    <div class="w-full grid grid-cols-4 gap-20">
-      <div v-for="i in 40" class="w-full h-full bg-green-50 rounded border-1 border-gray-200 flex p-10 items-center justify-center" :key="i">huy {{i}}</div>
+  <div class="fixed flex w-[280px] h-full">
+    чечня
+    вк хуйня
+    телега тоже
+  </div>
+  <div class="pl-[300px] flex h-screen">
+    <div class="w-full p-10 flex items-center justify-center">
+      <div class="w-full grid grid-cols-4 gap-20">
+        <div v-for="i in 80" class="w-full h-full bg-green-50 rounded border-1 border-gray-200 flex p-10 items-center justify-center" :key="i">huy {{i}}</div>
+      </div>
     </div>
   </div>
 </template>
