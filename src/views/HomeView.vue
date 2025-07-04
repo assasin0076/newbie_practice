60e61e0a537c9a184a9ccbe2476a0e2dfd3658f8
CUMBOOY <buravlev.1998@mail.ru>
2025-07-04 17:48:44 +0400

    feat: добавил верхнюю панель

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -10,7 +10,7 @@ const goHome = () => {
 </script>
 
 <template>
-  <div class="fixed flex flex-col w-[280px] h-full p-2 shadow-xl/30">
+  <div class="fixed top-0 left-0 flex flex-col w-[280px] h-full p-2 shadow-xl/30 z-[9999]">
     <div class="h-[200px] p-2">
       чечня
     </div>
@@ -23,10 +23,26 @@ const goHome = () => {
       </button>
     </div>
   </div>
+
+  <div class="fixed top-0 left-0 right-0 h-[70px] w-[calc(100%-280px)] translate-x-[280px] bg-cyan-900 grid grid-cols-4 gap-5 z-[50]">
+    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
+      Здесь
+    </div>
+    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
+      Могла быть
+    </div>
+    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
+      Ваша
+    </div>
+    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
+      Реклама
+    </div>
+  </div>
+
   <div class="pl-[300px] flex h-screen">
     <div class="w-full p-10 flex items-center justify-center">
       <div class="w-full grid grid-cols-4 gap-20">
-        <div v-for="i in 80" class="w-full h-full bg-green-50 rounded border-1 border-gray-200 flex p-10 items-center justify-center" :key="i">huy {{i}}</div>
+        <div v-for="i in 80" :key="i" class="w-full h-full bg-green-50 rounded border border-gray-200 flex p-10 items-center justify-center">huy {{i}}</div>
       </div>
     </div>
   </div>
