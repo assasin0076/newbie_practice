3a71c91912b9e7fdfb519a600115f5ea957ab22b
CUMBOOY <buravlev.1998@mail.ru>
2025-07-06 21:32:36 +0400

    feat: добавил на верхнюю панель две кнопки

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -23,21 +23,16 @@ const goHome = () => {
         Exit
       </button>
     </div>
-  </div>
-
-  <!-- Верхняя панель -->
-  <div class="fixed top-0 left-0 right-0 h-[70px] w-[calc(100%-280px)] translate-x-[280px] bg-cyan-900 grid grid-cols-4 gap-5 z-[50]">
-    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
-      Здесь
-    </div>
-    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
-      Могла быть
-    </div>
-    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
-      Ваша
-    </div>
-    <div class="p-4 items-center justify-center bg-cyan-500 shadow-lg shadow-cyan-500/50 ring-3 ring-cyan-700">
-      Реклама
+    <!-- Верхняя панель -->
+    <div class="fixed top-0 left-0 right-0 h-[70px] w-[calc(100%-280px)] translate-x-[280px] z-[50] p-2 shadow-xl bg-white flex justify-end items-end">
+      <div class="flex flex-col gap-1">
+        <button class="px-1 py-1 bg-pink-200 text-pink-800 font-medium rounded-full shadow-sm hover:bg-pink-300 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-opacity-50 transition duration-300 ease-in-out" @click="goHome">
+          Exit
+        </button>
+        <button class="px-1 py-1 bg-pink-200 text-pink-800 font-medium rounded-full shadow-sm hover:bg-pink-300 focus:outline-none focus:ring-2 focus:ring-pink-400 focus:ring-opacity-50 transition duration-300 ease-in-out" @click="goHome">
+          Exit
+        </button>
+      </div>
     </div>
   </div>
 
